dc5c09f17eb6c9eb2337a12faae211185f143a0a
Dekadee <dominic.ebner@outlook.com>
2020-07-06 14:18:47 +0200

    Fixed bilateral filtering, added comments and removed older implementation

--- src/SurfaceMeasurement.h
+++ src/SurfaceMeasurement.h
@@ -13,28 +13,56 @@ struct constants {
     Matrix3f *g_k_inv;
     float sigma_s;
     float sigma_r;
-    float wp;
 };
 
+/*
+ * Inline this function for better readability
+ */
 __device__ __forceinline__ float fancyN(float sigma, float t) {
     pow(M_E, -pow(t, 2) / pow(sigma, 2));
 }
 
+/*
+ * https://en.wikipedia.org/wiki/Bilateral_filter
+ * Unsure which pixels need to be chosen for smoothing, maybe its only the surrounding 8 (maybe more?)
+ * Wikipedia mentions the window centered in x
+ * Sigma_r represents the range parameter, increases smoothing range
+ * Sigma_s represents the spatial parameter, increases smoothing
+ * Parameter Wp is now computed alongside, still not sure if this is correct
+ */
 __device__ float
-computeDk(float *depthMap, size_t u, size_t v, float sigma_s, float sigma_r, float wp, size_t width, size_t N) {
+computeDk(float *depthMap, size_t u, size_t v, float sigma_s, float sigma_r, size_t width, size_t N) {
     float sum = 0.0f;
+    float sum2 = 0.0f;
 
     for (size_t i = 0; i < N; i++) {
         size_t u2 = i / width;
-        std::size_t v2 = i % width;
+        size_t v2 = i % width;
+        //Distance between pixels
         float t1 = sqrt(pow(u - u2, 2) + pow(v - v2, 2));
-        float t2 = abs(depthMap[u * width + v] - depthMap[i]);
-        sum += fancyN(sigma_s, t1) * fancyN(sigma_r, t2) * depthMap[i];
+        //This can be implented far more efficient, but depends heavily on sigma_r
+        if(t1 < 3 * sigma_r) {
+            //Difference of depth measurements
+            float t2 = abs(depthMap[u * width + v] - depthMap[i]);
+            //Smoothing factor
+            float fn_s = fancyN(sigma_s, t1);
+            //Range factor
+            float fn_r = fancyN(sigma_r, t2);
+            //Weight
+            float w = fn_s * fn_r;
+            sum += w * depthMap[i];
+            //Normalizing factor
+            sum2 += w;
+        }
     }
 
-    return sum / wp;
+    //Return normalized result
+    return sum / sum2;
 }
 
+/*
+ * Compute vertices for valid depth measurements
+ */
 __host__ __device__ void measureSurfaceVertices(
         float *depthMap,
         Vector3f *vertices,
@@ -53,11 +81,15 @@ __host__ __device__ void measureSurfaceVertices(
     size_t u = idx / width;
     size_t v = idx % width;
 
-    vertices[idx] = computeDk(depthMap, u, v, consts.sigma_s, consts.sigma_r, consts.wp, width, N) * consts.g_k_inv[0] *
+    //Back projection with filtered depth measurement
+    vertices[idx] = computeDk(depthMap, u, v, consts.sigma_s, consts.sigma_r, width, N) * consts.g_k_inv[0] *
                     Vector3f(u, v, 1);
 
 }
 
+/*
+ * Compute normals for previously computed vertices
+ */
 __host__ __device__ void
 measureSurfaceNormals(Vector3f *vertices, Vector3f *normals, size_t width, size_t height, size_t N) {
     size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -75,7 +107,11 @@ measureSurfaceNormals(Vector3f *vertices, Vector3f *normals, size_t width, size_
 
 class SurfaceMeasurement {
 
-    SurfaceMeasurement(Matrix3f k_inv, float sigma_s, float sigma_r, float wp, cudaStream_t stream = 0) {
+    /*
+     * Set up surface mesurement for specific camera and filtering parameters
+     * k_inv represents camera intrinsics
+     */
+    SurfaceMeasurement(Matrix3f k_inv, float sigma_s, float sigma_r, cudaStream_t stream = 0) {
 
         Matrix3f *g_k_inv;
 
@@ -86,39 +122,47 @@ class SurfaceMeasurement {
         consts.g_k_inv = g_k_inv;
         consts.sigma_s = sigma_s;
         consts.sigma_r = sigma_r;
-        consts.wp = wp;
 
     }
 
     /*
-     *  Sensor is only needed for sensor size, the rest should be pre-allocated, reusable GPU memory
+     *  Compute both vertices and normals for depth mesurements
+     *
+     *  Works on different resolution for the pyramid of LOD's
      *
      *  Returns it's result in the g_vertices and g_normals vector, kept in GPU memory
+     *  Validity map is not yet computed as
      */
-    void measureSurface(size_t width, size_t height, Vector3f *g_vertices, Vector3f *g_normals, float *g_depthMap,
+    void measureSurface(size_t width, size_t height, Vector3f *g_vertices, Vector3f *g_normals, float *g_depthMap, bool *g_validityMap,
                         cudaStream_t stream = 0) {
         size_t sensorSize = width * height;
 
         measureSurfaceVertices<<<(sensorSize + BLOCKSIZE - 1) / BLOCKSIZE, BLOCKSIZE, 0, stream >> > (
                 g_depthMap,
-                        g_vertices,
-                        consts,
-                        width,
-                        sensorSize
+                g_vertices,
+                consts,
+                width,
+                sensorSize
         );
+
+        CUDA_CHECK_ERROR
 
         size_t normalsSize = (width - 1) * (height - 1);
 
         measureSurfaceNormals<<<(sensorSize + BLOCKSIZE - 1) / BLOCKSIZE, BLOCKSIZE, 0, stream >> > (
                 g_vertices,
-                        g_normals,
-                        width - 1,
-                        height - 1,
-                        normalsSize
+                g_normals,
+                width - 1,
+                height - 1,
+                normalsSize
         );
+
+        CUDA_CHECK_ERROR
     }
 
-
+    /*
+     * Free GPU memory, only used in this class
+     */
     ~SurfaceMeasurement() {
         CUDA_CALL(cudaFree(consts.g_k_inv));
     }
